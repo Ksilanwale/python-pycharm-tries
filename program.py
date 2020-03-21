74b7fb62dede33dbede06d2b98eeb3b174757466
Ksilanwale <ksilrose@gmail.com>
2020-03-21 13:52:14 +0200

    New program file, my guess the number gamen

--- program.py
+++ program.py
@@ -1,1 +1,23 @@
-print("Hello Git")
+import random
+
+print()
+print("-------------------------")
+print("|    GUESS THE NUMBER   |")
+print("-------------------------")
+print()
+
+goal = random.randint(0, 100)
+number = -1
+
+player = input("Please enter your name ")
+
+while number != goal:
+    number_text = input("{}, guess a number between 0 and 100: ".format(player))
+    number = int(number_text)
+
+    if number > goal:
+        print("Sorry, {}, your number ({}) is too HIGH! try another one".format(player, number))
+    elif number < goal:
+        print("Sorry, {}, your number ({}) is too LOW! try another one".format(player, number))
+    else:
+        print("Excellent work, {}! {} is the right number!".format(player, goal))
